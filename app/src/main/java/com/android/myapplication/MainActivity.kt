a466893f6af64d59d2f7a496799fafffb3a4c4d0
Sreekanth K <sreekanth100k@gmail.com>
2020-08-22 23:37:53 +0530

    Sorting logic applied

--- app/src/main/java/com/android/myapplication/MainActivity.kt
+++ app/src/main/java/com/android/myapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.Exception
 import java.util.ArrayList
 
 
@@ -17,22 +18,18 @@ public class MainActivity:AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
+
         NetworkService.getInstance().jsonApi.fetchApiResponse().enqueue(object:Callback<TestResponse>{
             override fun onResponse(call: Call<TestResponse>, response: Response<TestResponse>) {
 
                 Log.d("onResponse",response.body().toString());
 
                 decodeResponseAndDoNecessaryActions(response)
-
-
-
             }
 
             override fun onFailure(call: Call<TestResponse>, t: Throwable) {
                 Log.d("onFailure","");
-
             }
-
         });
     }
 
@@ -41,8 +38,44 @@ public class MainActivity:AppCompatActivity() {
         val response: TestResponse? = response.body()
         Log.d("Response", response.toString())
 
-        var resultCount: String?         = response?.resultCount
-        var results: ArrayList<Results>? = response?.results
+        var resultCount: String?            =  response?.resultCount
+        var results: ArrayList<Results>?    =  response?.results
+        var resultsAfterTrackNameDupRemoval:ArrayList<Results> = ArrayList()
+        //Trackname#
+        //Remove duplicate data by Trackname#
+        if (results != null) {
+            for(i in 0..results.size-1){
+                var resultObj:Results   = results.get(i);
+                var isDuplicate:Boolean = false
+                for(j in 0..resultsAfterTrackNameDupRemoval.size-1){
+                    try {
+                        if (resultsAfterTrackNameDupRemoval.get(j).trackName == resultObj.trackName) {
+                            //Dont add, duplicate entry...
+                            isDuplicate = true;
+                        }
+                    }catch (e:Exception){
+                        Log.e("Exception",e.message.toString());
+                    }
 
+
+                }
+                if(!isDuplicate){
+                    resultsAfterTrackNameDupRemoval.add(resultObj);
+                }
+            }
+
+            Log.d("Response trkNm Removal", resultsAfterTrackNameDupRemoval.toString())
+
+            //Sort the data by release date in ascending order.
+
+
+            val resultsAfterSortingByReleaseDate = resultsAfterTrackNameDupRemoval.sortedWith(compareBy(Results::releaseDate))
+            Log.d("Response Sorting RlDate", resultsAfterSortingByReleaseDate.toString())
+
+
+
+
+
+        }
     }
 }
